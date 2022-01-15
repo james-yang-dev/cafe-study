dd4cef97110add3459f61936ba0689e297f744a4
carlos-yang <carlos@channel.io>
2022-01-15 10:10:54 +0900

    test

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,14 +1,13 @@
-import styled from '@emotion/styled';
-import { Link } from 'react-router-dom';
+import styled from "@emotion/styled";
+import { Link } from "react-router-dom";
 
 export function Header() {
   return (
     <HeaderStyled>
-      <Link to={'/order'}>order1/Link>
-      <Link to={'/detail'}>detail</Link>
+      <Link to={"/order"}>order1</Link>
+      <Link to={"/detail"}>detail</Link>
     </HeaderStyled>
   );
-};
+}
 
-const HeaderStyled = styled.div`
-`;
+const HeaderStyled = styled.div``;
